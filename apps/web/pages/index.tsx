deb14d4dea7d0c9c935df38881ca56b80a5029f4
Adam Woo <awoox2@gmail.com>
2024-11-24 10:26:29 -0500

    @acme -> @repo and create surfaces package

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { HomeScreen, HomeIcon } from '@acme/feature-home';
+import { HomeScreen, HomeIcon } from '@repo/feature-home';
 import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
